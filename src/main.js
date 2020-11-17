d018a9883f170a435621bfc1451815e2897ec778
Eric Endsley <eric.endsley4@gmail.com>
2020-11-17 14:13:23 -0800

    output dino ipsum as a list with breaks in between paragraphs
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,17 +6,20 @@ import DinoIpsum from './dino-ipsum.js';
 
 $(document).ready(function() {
   $('#results').click(function() {
-    let numberP = ('#paragraphs').val();
-    let numberW =('#words').val();
-    let promise = DinoIpsum.getDinos(numberP, numberW); {
-      promise.then(function(response) {
-        const body = JSON.parse(response); {
-          $('.output').text(`${body[0][1]}` );
-        },
-        function (error) {
-          $(".showErrors").text(`There was an error processing your request ${error}`);
-        }
-      });
-    }
+    let numberP = $('#paragraphs').val();
+    // clearFields();
+    let numberW = $('#words').val();
+    let promise = DinoIpsum.getDinos(numberP, numberW); 
+    promise.then(function(response) {
+      const body = JSON.parse(response); 
+      console.log(body);
+      for (let i = 0; i < body.length; i++) {
+        $('.output').append(`${body[i].join(" ")}` + `<br>` + `<br>`);
+      // $('.output').html(`${body.join(' ')}`);
+      }
+    }, 
+    function(error) {
+      $(".showErrors").text(`There was an error processing your request ${error}`);
+    });
   });
 });
